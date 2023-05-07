1bc1b323f0de7d0d6ca9b093676131c3caf3d3d8
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-05-07 17:00:23 -0300

    Department update Implementation test

--- src/aplication/Program2.java
+++ src/aplication/Program2.java
@@ -21,17 +21,23 @@ public class Program2 {
 		departmentDao.insert(dep);
 		System.out.println("Inserted! New id = " + dep.getId());
 		
-		System.out.println("=== Test 2: Department findById ===");
+		System.out.println("\n=== Test 2: Department findById ===");
 		dep = departmentDao.findById(2);
 		System.out.println(dep);
 		
 		
-		System.out.println("=== Test 3: Department findAll ===");	
+		System.out.println("\n=== Test 3: Department findAll ===");	
 		List<Department> depList = departmentDao.findAll();
 		for (Department obj : depList) {
 			System.out.println(obj);
 		}
 		
 		sc.close();
+		
+		System.out.println("\n=== Test 4: Department update ===");	
+		dep = departmentDao.findById(7);
+		dep.setName("Tecnology");
+		departmentDao.update(dep);
+		System.out.println("Update completed!");
 	}
 }
