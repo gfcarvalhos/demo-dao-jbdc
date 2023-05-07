c838e51dfbdfa35a289324c0d07b767ec735895e
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-05-07 09:56:44 -0300

    Implementing findById

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();   				//maceta para nao expor a implementação, apenas a interface.
+		return new SellerDaoJDBC(DB.getConnection());  //acerta para nao expor a implementação, apenas a interface.
 	}
 }
